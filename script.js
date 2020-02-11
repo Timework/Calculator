f9afbb724c6223289fb0cfdbb3cb0d6d0d6f1885
Timework <timework.bibo@gmail.com>
2020-02-11 14:27:32 -0600

    added basic functions and linked script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function sum(first, second) {
+    return first + second
+}
+function subtract(first, second) {
+    return first - second
+}
+function multiply(first, second) {
+    return first*second
+}
+function divide(first, second) {
+    return first/second
+}
