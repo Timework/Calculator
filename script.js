2074107e808b09109b9639cf93eabef61cabd8b9
Timework <timework.bibo@gmail.com>
2020-03-06 10:01:15 -0600

    I think the par works

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ let calArr = [];
 let calDisplay = "";
 
 function numberAdd(button) {
+    if (button.value === "("){
+        var x = button.value;
+        calDisplay += x;
+        document.getElementById("display").innerHTML = calValue;
+        document.getElementById("calDisplay").innerHTML = calDisplay;
+        calArr.push(x)
+        return
+    } 
     if (button.value === "-" && calValue.length === 0) {
         var x = button.value;
         calValue += x;
@@ -19,7 +27,9 @@ function numberAdd(button) {
     }
     while (calValue.charAt(0) === "0"){
         calValue = calValue.substring(1);
+        calDisplay = calDisplay.substring(0, calDisplay.length - 1);
         document.getElementById("display").innerHTML = calValue;
+        document.getElementById("calDisplay").innerHTML = calDisplay;
     }
 };
 
@@ -40,6 +50,15 @@ function backspace() {
 };
 
 function holdValue(button) {
+    if (calValue.indexOf(")") != -1) {
+        let f = countp(calValue);
+        calArr.push(parseFloat(calValue.substring(0, calValue.length - f)))
+        for (let o = 0; o < f; o++){
+            calArr.push(")");
+        }
+    } else {
+        calArr.push(parseFloat(calValue));
+    }
     if (calValue.length === 0 && calArr.length === 0) {
         calValue = "0";
         calDisplay += 0;
@@ -49,7 +68,6 @@ function holdValue(button) {
         document.getElementById("calDisplay").innerHTML = calDisplay;
         return calArr.push(button.value);
     }
-    calArr.push(parseFloat(calValue));
     calArr.push(button.value);
     calDisplay += button.value;
     calValue = "";
@@ -58,11 +76,20 @@ function holdValue(button) {
 };
 
 function runOperate(){
-    if (calValue === "-"){
+    if (calValue === "-") {
         return
     }
-    calArr.push(parseFloat(calValue));
-    calculator(calArr);
+    if (calValue.indexOf(")") != -1){
+        let f = countp(calValue);
+        calArr.push(parseFloat(calValue.substring(0, calValue.length - f)))
+        for (let o = 0; o < f; o++) {
+            calArr.push(")");
+        }
+    } else {
+        calArr.push(parseFloat(calValue));
+
+    }
+    calArr = calculator(calArr);
     calValue = calArr;
     calDisplay += "=" + calValue;
     document.getElementById("calDisplay").innerHTML = calDisplay;
@@ -73,39 +100,48 @@ function runOperate(){
     calDisplay = calValue;
 };
 
-function calculator(calArr) {
-    for (let i = 1; i < calArr.length; i += 2) {
-        if (calArr[i] === "*" || calArr[i] === "/") {
-            if (calArr[i] === "*") {
-                calArr[i] = calArr[i - 1] * calArr[i + 1];
-                calArr.splice(i - 1, 1);
-                calArr.splice(i, 1);
+function calculator(x) {
+    for (let h = 0; h < x.length; h++){
+        if (x[h] === "("){
+            x.splice(h, 1);
+            let g = looking(x.slice(h));
+            x.splice(h + g, 1);
+            let newValue = parseFloat(calculator(x.slice(h, h+g)))
+            x.splice(h, g, newValue);
+        }
+    }
+    for (let i = 0; i < x.length; i++) {
+        if (x[i] === "*" || x[i] === "/") {
+            if (x[i] === "*") {
+                x[i] = x[i - 1] * x[i + 1];
+                x.splice(i - 1, 1);
+                x.splice(i, 1);
                 i -= 2;
             } else {
-                calArr[i] = calArr[i - 1] / calArr[i + 1];
-                calArr.splice(i - 1, 1);
-                calArr.splice(i, 1);
+                x[i] = x[i - 1] / x[i + 1];
+                x.splice(i - 1, 1);
+                x.splice(i, 1);
                 i -= 2;
             }
         }
     }
 
-    for (let k = 1; k < calArr.length; k += 2) {
-        if (calArr[k] === "+" || calArr[k] === "-") {
-            if (calArr[k] === "+") {
-                calArr[k] = calArr[k - 1] + calArr[k + 1];
-                calArr.splice(k - 1, 1);
-                calArr.splice(k, 1);
+    for (let k = 0; k < x.length; k++) {
+        if (x[k] === "+" || x[k] === "-") {
+            if (x[k] === "+") {
+                x[k] = x[k - 1] + x[k + 1];
+                x.splice(k - 1, 1);
+                x.splice(k, 1);
                 k -= 2;
             } else {
-                calArr[k] = calArr[k - 1] - calArr[k + 1];
-                calArr.splice(k - 1, 1);
-                calArr.splice(k, 1);
+                x[k] = x[k - 1] - x[k + 1];
+                x.splice(k - 1, 1);
+                x.splice(k, 1);
                 k -= 2;
             }
         }
     }
-    calArr[0] = (Math.round(calArr[0] * 10000) / 10000).toFixed(4)
+    return x[0] = (Math.round(x[0] * 10000) / 10000).toFixed(4)
 };
 
 function deciAdd(button) {
@@ -146,4 +182,28 @@ function historyMove(){
             document.getElementById(`historyd${i + 1}`).innerHTML = document.getElementById(`historyd${i}`).innerHTML
         }
     }
+}
+function looking(x){
+    let finder = 1;
+    for (let i = 0; i < x.length; i++){
+        if (x[i] === "("){
+            finder += 1;
+        }
+        if (x[i] === ")"){
+            finder -= 1;
+        }  
+        if (finder === 0){
+            return i
+        }
+    }
+}
+function countp(x){
+    let counter = 0;
+    xsplit = x.split("");
+    for (let i = 0; i < xsplit.length; i++){
+        if (xsplit[i] === ")"){
+            counter += 1
+        }
+    }
+    return counter
 }
